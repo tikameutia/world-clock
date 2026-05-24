6e8e1949b798ff07affbcf5695fa0b77c9b8f5ae
Tika Meutia Lubis <111536774+tikameutia@users.noreply.github.com>
2026-05-24 09:51:13 +0200

    Created function to update the time display based on the city selected

--- src/app.js
+++ src/app.js
@@ -20,3 +20,19 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCityDisplay(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let main = document.querySelector("main");
+  main.innerHTML = `<div class="city-display">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${moment().tz(cityTimeZone).format("ddd, D MMMM YYYY")}</div>
+          </div>
+          <div class="time-display">${moment().tz(cityTimeZone).format("hh:mm:ss [<small>]A[</small>]")}</div>
+        </div>`;
+}
+
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", updateCityDisplay);
